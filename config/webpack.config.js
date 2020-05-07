a1e36fcee0fef419af49d3b84ff897caae7418a7
Sudeep G <sudeep@coffeebeans.io>
2020-05-07 12:52:58 +0530

    Added bundle-analyzer-plugin

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -24,6 +24,7 @@ const getClientEnvironment = require('./env');
 const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const postcssNormalize = require('postcss-normalize');
 
@@ -633,6 +634,12 @@ module.exports = function (webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
         }),
+        isEnvProduction &&
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            reportFilename: '__bundle_analyzer_report.html',
+            openAnalyzer: false
+          }),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
